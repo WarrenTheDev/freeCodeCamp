0e68bcea57253c583014e702533e5f770a756fb5
WarrenTheDev <122322119+WarrenTheDev@users.noreply.github.com>
2023-03-06 14:44:00 -0500

    started check register algo
    need to add while Loop

---
+++ FreeCodeCampAlgo/checkCashRegister.js
@@ -0,0 +1,41 @@
+function checkCashRegister(price, cash, cid) {
+  let changeNeeded = cash - price;
+  let status = "";
+  let changeToGive = [];
+
+  const currUnit = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
+  };
+
+  cid.reduceRight((prevVal, currVal) => {
+    if (change / currUnit[currVal[0]] >= 1) {
+      let leftOver = currVal[1];
+      //while loop here
+      console.log(leftOver);
+    }
+  }, cid[cid.length - 1]);
+
+  return changeNeeded;
+}
+
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
